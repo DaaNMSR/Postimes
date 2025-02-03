045787bc72656e7aa7a36eea1df0a4a578e747e6
DaaNMSR <denispresent4@gmail.com>
2025-02-03 12:06:14 +0300

    refactoring 2.3

--- src/pages/FavoritesPage/components/NoFavorites/NoFavorites.tsx
+++ src/pages/FavoritesPage/components/NoFavorites/NoFavorites.tsx
@@ -5,6 +5,7 @@ export const NoFavorites = () => {
   return (
     <div className="grid place-items-center">
       <p className="text-2xl my-4">No favorites.</p>
+
       <Link
         to="/githubsearch"
         className="p-4 hover:bg-gray-400 hover:text-white border-2 border-gray-700"
